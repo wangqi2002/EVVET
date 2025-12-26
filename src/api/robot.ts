b0b06b39cd23a8bf2d09063754b38d9a2ea3b25d
wangqi2002 <1722009706@qq.com>
2025-12-26 16:26:40 +0800

    语音传入后端

--- src/api/robot.ts
+++ src/api/robot.ts
@@ -1,6 +1,10 @@
 import type { LoginResponse, Page, QueryRequest, User } from '../typings/types'
 import requestWithToken from '../utils/request'
 
-export function getRobot(data?: any) {
-    return requestWithToken<string>('/robot', 'GET', data)
+export function getReply(data?: any) {
+    return requestWithToken<string>('/robot/reply', 'GET', data)
+}
+
+export function getText(data?: any) {
+    return requestWithToken<string>('/robot/text', 'GET', data)
 }
--- src/components/chat/MessageInput.vue
+++ src/components/chat/MessageInput.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
-import { Position } from "@element-plus/icons-vue";
+import { Position, Microphone } from "@element-plus/icons-vue";
 import { ref } from "vue";
-import { useXfAsr } from "~/utils/useXfAsr.js";
 import emitter from "~/utils/bus";
-import { getRobot } from "~/api/robot";
+import { getReply, getText } from "~/api/robot";
 import dayjs from "dayjs";
+import Recorder from 'js-audio-recorder';
 
 // 发送消息消息事件
 const emit = defineEmits<{
@@ -15,12 +15,18 @@ const props = defineProps<{
     isInSession: () => boolean;
 }>();
 
-const { startRecording, stopRecording, recordText, resultText } = useXfAsr();
-
 // 输入框内的消息
 const message = ref("");
+
 const isListening = ref(false);
 const timeOutEvent = ref<ReturnType<typeof setTimeout> | null>(null);
+const record = new Recorder({
+    sampleBits: 16, // 采样位数，支持 8 或 16，默认是16
+    sampleRate: 16000, // 采样率，支持 11025、16000、22050、24000、44100、48000，根据浏览器默认值，我的chrome是48000
+    numChannels: 1, // 声道，支持 1 或 2， 默认是1
+    // compiling: false,(0.x版本中生效,1.x增加中)  // 是否边录边转换，默认是false
+})
+
 async function sendMessage() {
     if (!props.isInSession()) {
         emitter.emit("createNewSession");
@@ -29,7 +35,7 @@ async function sendMessage() {
     const userMessage = message.value;
     const requestValue = { question: message.value }
     message.value = "";
-    const data = await getRobot(requestValue); // 获取答复内容
+    const data = await getReply(requestValue); // 获取答复内容
     // emitter.emit("debugMS", data);
     // const data = "这是机器人回复的示例内容。"; // 示例回复内容
     emitter.emit("addReplyMessage", { userMessage, replyMessage: data }); // 发送事件
@@ -37,21 +43,45 @@ async function sendMessage() {
 function goTouchstart() {
     timeOutEvent.value = setTimeout(() => {
         isListening.value = true;
-        startRecording();
+        startRecordAudio();
     }, 200); // 长按200毫秒后，触发长按事件
 }
 // 手如果在200毫秒内就释放，则取消长按事件
 async function goTouchend() {
-    if (timeOutEvent.value) {
-        clearTimeout(timeOutEvent.value);
+    if (isListening.value) {
+        console.log("取消录音");
         timeOutEvent.value = null;
-        await stopRecording();
+        record.stop();
+        isListening.value = false;
+        const wavBlob = record.getWAVBlob();
+        const requestValue = { audio: wavBlob }
+        // console.log(requestValue);
+        emitter.emit("debugMS", requestValue);
+        const data = await getText(requestValue)
+        // console.log(data);
+        emitter.emit("debugMS", data);
         setTimeout(() => {
-            message.value = resultText.value;
+            message.value = data.result;
             // emitter.emit("debugMS", resultText.value);
         }, 200);
     }
 }
+
+function startRecordAudio() {
+    Recorder.getPermission().then(
+        () => {
+            console.log("开始录音");
+            record.start(); // 开始录音
+        },
+        (error: any) => {
+            // alert("无法获取麦克风权限");
+            isListening.value = false;
+            console.log(`${error.name} : ${error.message}`);
+        }
+    );
+}
+
+
 </script>
 
 <template>
@@ -64,9 +94,9 @@ async function goTouchend() {
                 <el-button type="primary" @mousedown.prevent="goTouchstart()" @mouseup.prevent="goTouchend()"
                     @touchstart.prevent="goTouchstart()" @touchend.prevent="goTouchend()">
                     <el-icon class="el-icon--left">
-                        <Position />
+                        <Microphone />
                     </el-icon>
-                    {{ recordText }}
+                    {{ isListening ? '正在录音...' : '按住说话' }}
                 </el-button>
                 <el-button type="primary" @click="sendMessage">
                     <el-icon class="el-icon--left">
